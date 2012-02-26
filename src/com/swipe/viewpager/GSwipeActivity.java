41d998080c7489ba6b78aaf221b1eb62a89c5c5d
Snehal <Snehal@cyllene.local>
2012-02-25 20:18:24 -0500

    initial setup

---
+++ src/com/swipe/viewpager/GSwipeActivity.java
@@ -0,0 +1,24 @@
+package com.swipe.viewpager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+public class GSwipeActivity extends Activity {
+    /** Called when the activity is first created. */
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+	    super.onCreate( savedInstanceState );
+	    setContentView( R.layout.main );
+	 
+	    ViewPagerAdapter adapter = new ViewPagerAdapter( this );
+	    ViewPager pager =
+	        (ViewPager)findViewById( R.id.viewpager );
+	    TitlePageIndicator indicator =
+	        (TitlePageIndicator)findViewById( R.id.indicator );
+	    pager.setAdapter( adapter );
+	    indicator.setViewPager( pager );
+	    
+	}
+}
---
+++ src/com/swipe/viewpager/Mew.java
@@ -0,0 +1,20 @@
+package com.swipe.viewpager;
+
+public class Mew {
+
+	public static void main(String[] args) {
+		System.out.println("asdasd\n");
+		double h = 3 + 2 / 5 + 2;
+		System.out.println("asd\n");
+		System.out.println("assad\n" + h);
+
+		int i = 0;
+
+		switch (i) {
+		case 1:
+			System.out.println("");
+		default:
+			System.out.println("");
+		}
+	}
+}
